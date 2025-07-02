7e49acc0d7d1ae648153341953bbee761cfb8f2c
foxsportscomactivate <radhjain1@gmail.com>
2025-07-02 14:09:25 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -16,7 +16,7 @@ import sys
 
 # -- Project information -----------------------------------------------------
 
-project = 'HP Smart'
+project = 'Setup Canon Printer'
 
 
 # The full version, including alpha/beta/rc tags
